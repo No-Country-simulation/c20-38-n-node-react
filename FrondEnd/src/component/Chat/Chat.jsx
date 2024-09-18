89ee7b6b53dc2d90fe4a41da61fb8fccb74ec1c2
aguusoleti <agustinsoleti95@gmail.com>
2024-09-17 23:15:25 -0300

    chat andando

--- FrondEnd/src/component/Chat/Chat.jsx
+++ FrondEnd/src/component/Chat/Chat.jsx
@@ -1,22 +1,21 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Users from '../User/personas.json';
 import io from "socket.io-client";
-import { useUser } from '../../hooks/useUser';
 
 export default function App() {
   const [selectedUser, setSelectedUser] = useState(undefined);
-  const { userData } = useUser();
   const [token, setToken] = useState(null);
   const [socket, setSocket] = useState(null);
   const [chatsData, setChatsData] = useState([]);
   const [messages, setMessages] = useState([]);
   const [id_chat, setIdChat] = useState("");
   const [messageText, setMessageText] = useState("");
+  const [allUser, setallUser] = useState([]);
+  const [ownDataUser, setownDataUser] = useState()
 
   const url = "https://inative-back.onrender.com";
 
-  // Obtener el token del localStorage solo en el lado del cliente
+
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
     setToken(storedToken);
@@ -43,24 +42,12 @@ export default function App() {
     }
   }, [token]);
 
-  useEffect(() => {
-    (async () => {
-      await fetch(`${url}/getChatsByUserId/1`)
-        .then((response) => response.json())
-        .then((result) => {
-          console.log("Respuesta de la API de chats:", result); // Ver la respuesta en la consola
-          setChatsData(result); // Actualiza el estado con los datos de la API
-        })
-        .catch((error) => console.error("Error al obtener los chats:", error));
-    })();
-  }, []);
+
 
   useEffect(() => {
     if (socket) {
       socket.on("message", (newMessage) => {
-        console.log("Nuevo mensaje recibido:", newMessage);
-        setMessages((prevMessages) => [...prevMessages, newMessage]);
-        console.log("Hola");
+        setMessages(newMessage);
       });
 
       return () => {
@@ -76,19 +63,79 @@ export default function App() {
       setMessages([]);
     }
   };
-
+console.log(chatsData);
   const sendMessage = (event) => {
     event.preventDefault();
-    if (messageText.trim() !== "" && socket) {
+    const dataLogin = Object.fromEntries(new FormData(event.target));
+
+    if (socket) {
       socket.emit("message", {
         id_chat: id_chat,
-        message: messageText,  // Enviar el texto del mensaje
+        message: dataLogin,
       });
-      // Limpiar el campo de texto después de enviar el mensaje
-      setMessageText("");
     }
+
+    setTimeout(() => {
+      ingresarAlChat(id_chat);
+    }, 100);
   };
 
+  const getChatByUserId = async (id) => {
+    await fetch(`${url}/getChatsByUserId/${id}`)
+      .then((response) => response.json())
+      .then((result) => setChatsData(result))
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      fetchUser(token);
+      (
+        async () => {
+          try {
+            const response = await fetch(`https://inative-back.onrender.com/getUserByToken/${token}`, {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+
+            if (!response.ok) {
+              throw new Error("Error al traer los datos");
+            }
+
+            const result = await response.json();
+            setownDataUser(result)
+            getChatByUserId(result?.userToken.id_user)
+            // Redirigir al chat
+          } catch (error) {
+            console.error("Error al traer los datos:", error);
+          }
+        })()
+    }
+
+  }, []);
+  const fetchUser = async (token) => {
+    try {
+      const response = await fetch("https://inative-back.onrender.com/getAllUser", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setallUser(data);
+      } else {
+        console.error("Error fetching user data:", response.status, response.statusText);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
   return (
     <div className="flex justify-center mt-28">
       <div className="flex flex-col bg-white border border-gray-300 rounded-lg p-4 w-[383px] max-h-[710px] mb-28">
@@ -96,23 +143,23 @@ export default function App() {
           <h2>Historial</h2>
         </div>
         <div className="flex flex-col space-y-2 max-h-[700px] overflow-y-auto">
-          {Users.map((user, index) => (
+          {chatsData.map((chat, index) => (
             <div
               key={index}
               className="flex items-center space-x-4 cursor-pointer"
-              onClick={() => handleUserClick(user)}
+              onClick={() => { handleUserClick(chat.message_x_chats[0]?.message.user), ingresarAlChat(chat.id_chat) }}
             >
               <img
-                src={user.foto}
+                src={chat.message_x_chats[0]?.message.user.avatar}
                 alt="User Photo"
                 className="w-10 h-10 rounded-full border border-gray-300"
               />
               <div className="flex-1">
-                <h3 className="text-blue-600">{user.nombre}</h3 >
+                <h3 className="text-blue-600">{chat.message_x_chats[0]?.message.user.full_name}</h3 >
               </div>
               <div className="flex items-center space-x-1">
                 <div
-                  className={`w-3 h-3 rounded-full ${user.estado === 'Activo' ? 'bg-green-500' : 'bg-gray-500'}`}
+                  className={`w-3 h-3 rounded-full ${chat.message_x_chats[0]?.message.user.status === 'connected' ? 'bg-green-500' : 'bg-gray-500'}`}
                 ></div>
               </div>
             </div>
@@ -125,15 +172,15 @@ export default function App() {
           {selectedUser ? (
             <div className="flex items-center justify-between h-[71px] w-full bg-white border border-gray-300 rounded-lg p-4">
               <img
-                src={selectedUser.foto}
+                src={selectedUser.avatar}
                 alt="User Photo"
                 className="w-12 h-12 rounded-full border border-gray-300"
               />
               <div className="flex-1 ml-4">
-                <h3 className="font-bold text-lg">{selectedUser.nombre}</h3>
+                <h3 className="font-bold text-lg">{selectedUser.user_name}</h3>
               </div>
               <div
-                className={`w-3 h-3 rounded-full ${selectedUser.estado === 'Activo' ? 'bg-green-500' : 'bg-gray-500'}`}
+                className={`w-3 h-3 rounded-full ${selectedUser.status === 'connected' ? 'bg-green-500' : 'bg-gray-500'}`}
               ></div>
             </div>
           ) : (
@@ -141,44 +188,39 @@ export default function App() {
           )}
         </div>
 
-        <div className="bg-white border border-gray-300 rounded-lg p-4 flex flex-col h-[929px] w-[925px] ml-4">
-          <div className="flex flex-col flex-1 overflow-auto">
-            {chatsData?.map((chat, index) => (
-              <div key={index} className="flex flex-col text-white bg-zinc-900 p-2">
-                {chat.message_x_chats?.[0]?.message?.user?.full_name ? (
-                  <div onClick={() => ingresarAlChat(chat.id_chat)} className="cursor-pointer">
-                    <div>{chat.message_x_chats[0].message.user.full_name} :</div>
-                    <div>{chat.message_x_chats[0].message.message}</div>
-                  </div>
-                ) : (
-                  <div>No hay más mensajes disponibles</div>
-                )}
-              </div>
-            ))}
-
-            {messages?.map((message, index) => (
-              <li key={index} className="my-2 p-2 table text-sm rounded-md bg-black">
-                <span style={{ color: "red" }}>{message.message.user.user_name}</span>: {message.message.message}
-              </li>
-            ))}
+        <div className="bg-white border border-gray-300 rounded-lg p-4 flex flex-col h-[929px] w-[925px] mt-10 ml-4">
+          <div className="flex flex-col flex-1 overflow-auto mt">
+            {messages?.data?.map((message, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`text ${message?.message?.user?.id_user !== messages?.id_user
+                    ? 'text-start bg-[#E7E7E7] my-2 text-sm  mb-5 p-3 rounded-lg mr-auto'
+                    : 'text-end ml-auto  bg-background-chat my-2 text-sm  mb-5 p-3 rounded-lg '
+                    } `}
+                >
+                  <div className=" w-fit ">
+                    {message.message.message}
+                    </div>
+                </div>
+              );
+            })}
           </div>
 
           <form onSubmit={sendMessage} className="flex items-center mt-4 border-t border-gray-300 pt-2">
             <input
-              type="text"
               name="message"
+              type="text"
               className="flex-1 border border-gray-300 rounded-lg px-4 py-2 mr-2"
               placeholder="Escribe un mensaje..."
-              value={messageText}
-              onChange={(e) => setMessageText(e.target.value)} // Actualizar el estado con el texto del mensaje
               autoFocus
             />
             <button type="submit" className="bg-background-enviar text-white px-4 py-2 rounded-lg">
               Enviar
             </button>
-
           </form>
         </div>
+
       </div>
     </div>
   );
